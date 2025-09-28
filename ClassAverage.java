abdcbee9876d1fb5b58ef66a6937dafd7546ebc7
mlstauff <stauffm@rider.edu>
2025-09-28 13:48:15 -0400

    ClassAverage.java

---
+++ ClassAverage.java
@@ -0,0 +1,29 @@
+// Fig. 3.1: ClassAverage.java
+// Solving the class-average problem using counter-controlled iteration.
+import java.util.Scanner; // program uses class Scanner
+
+public class ClassAverage {
+  public static void main(String[] args) {
+    // create Scanner to obtain user input
+    Scanner input = new Scanner(System.in);
+
+    // initialization phase
+    int total = 0; // initialize sum of grades entered by the user
+    int gradeCounter = 1; // initialize grade # to be entered next
+
+    // processing phase uses counter-controlled iteration
+    while (gradeCounter <= 10) { // loop 10 times
+      System.out.print("Enter grade: "); // prompt for next grade
+      int grade = input.nextInt(); // input the grade
+      total = total + grade; // add grade to total
+      gradeCounter = gradeCounter + 1; // increment counter by 1
+    }
+
+  // termination phase
+  int average = total / 10; // integer division yields integer result
+
+  // display total and average of grades
+  System.out.printf("%nTotal of all 10 grades is %d%n", total);
+  System.out.printf("Class average is %d%n", average);
+  }
+}
